cf473a2f2d6b6bd5272d88f07d54f395d8ae8aac
greg <mynameisgregwilk@gmail.com>
2017-02-25 17:44:04 -0600

    colors

--- app/src/main/java/com/example/t_ste/resumekings/Web_Rest_API.java
+++ app/src/main/java/com/example/t_ste/resumekings/Web_Rest_API.java
@@ -18,7 +18,9 @@ import cz.msebera.android.httpclient.protocol.HTTP;
 
 public class Web_Rest_API {
 
-    private static final String BASE_URL = "RIP";
+
+
+    private static final String BASE_URL = "http://138.197.85.203:3002/resume/";
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
